a170fd30a948345692a4a662309b38d8f6d5c2ea
nathangrr <nathanranthony@gmail.com>
2020-08-20 11:05:09 -0400

    make it better

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,8 +4,14 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        //filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        "client": "postgres",
+        "host": "${process.env.DATABASE_HOST}",
+        "port": "${process.env.DATABASE_PORT}",
+        "database": "${process.env.DATABASE_NAME}",
+        "username": "${process.env.DATABASE_USERNAME}",
+        "password": "${process.env.DATABASE_PASSWORD}",
+        "ssl": { "rejectUnauthorized": false }
       },
       options: {
         useNullAsDefault: true,
@@ -13,3 +19,6 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+
+// postgres://jhaxxwfhdnorng:b2cf13f495e97db76be7ea4d7288f1510c796ffe450b7de3dbbd7eca9fc77fee@ec2-3-217-87-84.compute-1.amazonaws.com:5432/d1t059akm8ls6c
