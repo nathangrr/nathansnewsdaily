1eb07d97ef59b6a37485e7f7e09b192a0e1d1e78
nathangrr <nathanranthony@gmail.com>
2020-08-20 16:27:35 -0400

    first commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,25 +4,19 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        //filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        //client: "sqlite"
-        "client": "postgres",
-        "host": "${process.env.DATABASE_HOST}",
-        "port": "${process.env.DATABASE_PORT}",
-        "database": "${process.env.DATABASE_NAME}",
-        "username": "${process.env.DATABASE_USERNAME}",
-        "password": "${process.env.DATABASE_PASSWORD}",
-        "ssl": { "rejectUnauthorized": false }
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "sqlite"
+        //"client": "postgres",
+        //"host": "${process.env.DATABASE_HOST}",
+        //"port": "${process.env.DATABASE_PORT}",
+        //"database": "${process.env.DATABASE_NAME}",
+        //"username": "${process.env.DATABASE_USERNAME}",
+        //"password": "${process.env.DATABASE_PASSWORD}",
+        //"ssl": { "rejectUnauthorized": false }
       },
       options: {
         useNullAsDefault: true,
       },
-      scripts: {
-        "start": "node index.js"
-      },
     },
   },
 });
-
-
-// postgres://jhaxxwfhdnorng:b2cf13f495e97db76be7ea4d7288f1510c796ffe450b7de3dbbd7eca9fc77fee@ec2-3-217-87-84.compute-1.amazonaws.com:5432/d1t059akm8ls6c
